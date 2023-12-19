eec29e7469f232ca65d72708d8df0035f1f5359d
Williams Caballero Araya <williams.caballeroa@gmail.com>
2023-12-19 08:57:24 -0300

    feat: Agregar commit test

--- src/app.js
+++ src/app.js
@@ -3,3 +3,4 @@ console.log("Otro cambio en el proyecto");
 console.log("Tercer commit de prueba");
 console.log("Agregar skip bump");
 console.log("Subir cambios: git push origin master");
+console.log("Commit test");
