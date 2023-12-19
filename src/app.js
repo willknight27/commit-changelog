f1dfcef2fb5a06c737d1869bb225084b8944f663
Williams Caballero Araya <williams.caballeroa@gmail.com>
2023-12-18 21:10:29 -0300

    feat: Subir cambios: git push origin master

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ console.log("Hello World!!");
 console.log("Otro cambio en el proyecto");
 console.log("Tercer commit de prueba");
 console.log("Agregar skip bump");
+console.log("Subir cambios: git push origin master");
